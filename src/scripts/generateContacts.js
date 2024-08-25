64e5021b43f54c1626c8f77eead21a79b8dae94e
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-25 11:10:23 +0300

    add code 1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import createFakeContact from '../utils/createFakeContact.js';
+import * as fs from 'node:fs/promises';
+import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const { encoding } = await DetectFileEncodingAndLanguage(PATH_DB);
+  const contactsList = JSON.parse(await fs.readFile(PATH_DB, encoding));
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  const data = [...contactsList, ...newContacts];
+  await fs.writeFile(PATH_DB, JSON.stringify(data, null, 2));
+};
 
 generateContacts(5);
