c71e94f1191a4f926669eec311a6bbc88147dabc
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-25 12:01:49 +0300

    add code 2

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,10 @@
-import { PATH_DB } from '../constants/contacts.js';
+// import { PATH_DB } from '../constants/contacts.js';
+// import * as fs from 'node:fs/promises';
+import getAllContacts from './getAllContacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  const contacts = await getAllContacts();
+  return contacts.length;
+};
 
 console.log(await countContacts());
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,11 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
 import createFakeContact from '../utils/createFakeContact.js';
 import * as fs from 'node:fs/promises';
-import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
+// import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
+import getAllContacts from './getAllContacts.js';
 
 const generateContacts = async (number) => {
-  const { encoding } = await DetectFileEncodingAndLanguage(PATH_DB);
-  const contactsList = JSON.parse(await fs.readFile(PATH_DB, encoding));
+  //   const { encoding } = await DetectFileEncodingAndLanguage(PATH_DB);
+  //   const contactsList = JSON.parse(await fs.readFile(PATH_DB, encoding));
+  const contactsList = await getAllContacts();
   const newContacts = Array(number).fill(0).map(createFakeContact);
   const data = [...contactsList, ...newContacts];
   await fs.writeFile(PATH_DB, JSON.stringify(data, null, 2));
