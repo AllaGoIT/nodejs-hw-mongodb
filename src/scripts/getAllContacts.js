ff8ea365598568a0ac81ac026c95a39b8347f0e5
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-09-04 12:50:01 +0300

    add start code

--- src/scripts/getAllContacts.js
+++
@@ -1,9 +0,0 @@
-import { PATH_DB } from '../constants/contacts.js';
-import * as fs from 'node:fs/promises';
-const getAllContacts = async () => {
-  const data = await fs.readFile(PATH_DB, 'utf-8');
-  return JSON.parse(data);
-};
-export default getAllContacts;
-// console.log(await getAllContacts());
-getAllContacts();
