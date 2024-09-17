fff5b5032054c44175f65f7496c9362642dc6948
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-09-17 12:14:18 +0300

    add code fix cell

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,5 +1,5 @@
 const calculatePaginationData = ({ count, perPage, page }) => {
-  const totalPages = Math.cell(count / perPage);
+  const totalPages = Math.ceil(count / perPage);
   const hasNextPage = page < totalPages;
   const hasPreviousPage = page !== 1;
   return {
