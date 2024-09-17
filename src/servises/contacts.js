324f2d772d6cbcb19fe110544c0472e0cd88ac86
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-09-17 13:31:11 +0300

    add code fix name to id

--- src/servises/contacts.js
+++ src/servises/contacts.js
@@ -5,7 +5,7 @@ import { SORT_ORDER } from '../constants/index.js';
 export const getContacts = async ({
   perPage,
   page,
-  sortBy = 'name',
+  sortBy = '_id',
   sortOrder = SORT_ORDER[0],
 }) => {
   const skip = (page - 1) * perPage;
