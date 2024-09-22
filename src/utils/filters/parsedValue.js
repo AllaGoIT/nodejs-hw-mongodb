81d14afc7c50cb53a3ecd7397d6f842336308956
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-09-22 14:12:44 +0300

    add fix 2 filter

--- src/utils/filters/parsedValue.js
+++ src/utils/filters/parsedValue.js
@@ -1,7 +1,7 @@
 export const parsedContactType = (type) => {
   const contactType = typeof type === 'string';
   if (!contactType) return;
-  const isType = (type) => ['personel', 'female', 'other'].includes(type);
+  const isType = (type) => ['personal', 'home'].includes(type);
   if (isType(type)) return type;
 };
 export const parsedIsFavourite = (type) => {
