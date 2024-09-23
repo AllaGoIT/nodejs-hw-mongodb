11a88227add5ff516bdee3072e25f49627444228
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-09-23 09:43:25 +0300

    add code 3 hw 5

--- src/servises/auth.js
+++ src/servises/auth.js
@@ -1,6 +1,13 @@
+import createHttpError from 'http-errors';
 import userCollection from '../db/models/User.js';
 
 export const signup = async (payload) => {
+  const { email } = payload;
+  const user = await userCollection.findOne({ email });
+  if (user) {
+    throw createHttpError(409, 'Email in use');
+  }
+
   const data = await userCollection.create(payload);
   delete data._doc.password;
   return data._doc;
