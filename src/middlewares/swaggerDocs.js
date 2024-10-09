4f9ae2e16dc1560bffeaa2b3ae0a74ffd6d72a3b
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-09 13:14:12 +0300

    add code fix swaggerDoc

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -5,8 +5,9 @@ import { SWAGGER_PATH } from '../constants/index.js';
 
 const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return [...swaggerUI.serve, ...swaggerUI.setup(swaggerDoc)];
+    const swaggerContent = fs.readFileSync(SWAGGER_PATH, 'utf-8');
+    const swaggerData = JSON.parse(swaggerContent);
+    return [...swaggerUI.serve, ...swaggerUI.setup(swaggerData)];
   } catch {
     return (req, res, next) =>
       next(createHttpError(500, "Can't load swagger docs"));
